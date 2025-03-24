56a0561a1664cd5692b4623903d230d61c282914
Jacquin Mininger <jacquin.mininger@gmail.com>
2025-03-24 14:08:26 -0400

    fix: two projects needed different compiler options and separating them like this was easiest

--- client/index.ts
+++ client/index.ts
@@ -1,9 +1,9 @@
 import Client from "@triton-one/yellowstone-grpc";
 
-const serverEndpoint = process.env.SERVER_ENDPOINT || "https://localhost:10000";
+const serverEndpoint = process.env.SERVER_ENDPOINT || "http://localhost:10000";
 // const token = process.env.TOKEN || "<your-token>";
 
 const client = new Client(serverEndpoint, undefined, undefined);
 const version = await client.getVersion();
-console.log("HELLO WORLD");
+console.log("Version: ", version)
 // grpc.runClient(client).then(console.log).catch(console.error);
