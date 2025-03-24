70ca8fe6257d7e16cbe6b8edf5024b6b97431d58
Jacquin Mininger <jacquin.mininger@gmail.com>
2025-03-24 15:24:35 -0400

    basic client subscription

--- client/index.ts
+++ client/index.ts
@@ -1,4 +1,10 @@
-import Client from "@triton-one/yellowstone-grpc";
+import Client, {
+  CommitmentLevel,
+  SubscribeRequest,
+  SubscribeRequestFilterAccountsFilter,
+} from "@triton-one/yellowstone-grpc";
+
+const ACCOUNT_PK = "AVyADfkTrCpL49w2hp3hy7oLzY4iijsMxeQZEdj8mRDs"
 
 const serverEndpoint = process.env.SERVER_ENDPOINT || "http://localhost:10000";
 // const token = process.env.TOKEN || "<your-token>";
@@ -7,3 +13,63 @@ const client = new Client(serverEndpoint, undefined, undefined);
 const version = await client.getVersion();
 console.log("Version: ", version)
 // grpc.runClient(client).then(console.log).catch(console.error);
+
+// Subscribe for events
+const stream = await client.subscribe();
+
+// Create `error` / `end` handler
+const streamClosed = new Promise<void>((resolve, reject) => {
+  stream.on("error", (error) => {
+    reject(error);
+    stream.end();
+  });
+  stream.on("end", () => {
+    resolve();
+  });
+  stream.on("close", () => {
+    resolve();
+  });
+});
+
+// Handle account updates
+stream.on("data", (data) => {
+  if (data.account) {
+    const accountInfo = data.account;
+    console.log('Account Update:', accountInfo);
+  }
+});
+
+const request: SubscribeRequest = {
+    accounts: {
+      client: {
+        owner: ["Stake11111111111111111111111111111111111111"],
+        account: [ACCOUNT_PK],
+        filters: [], // You can add additional filters here if needed
+      }
+    },
+    slots: {},
+    transactions: {},
+    transactionsStatus: {},
+    entry: {},
+    blocks: {},
+    blocksMeta: {},
+    commitment: undefined,
+    accountsDataSlice: [],
+    ping: undefined,
+  };
+
+  // Send subscribe request
+await new Promise<void>((resolve, reject) => {
+  stream.write(request, (err: Error|null) => {
+    if (err === null || err === undefined) {
+      resolve();
+    } else {
+      reject(err);
+    }
+  });
+}).catch((reason) => {
+  console.error(reason);
+  throw reason;
+});
+
+await streamClosed;
